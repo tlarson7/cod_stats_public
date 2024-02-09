770e06d839916ed7eabc46115028c97327592f46
Tanner Larson <65421823+tlarson7@users.noreply.github.com>
2024-02-09 05:28:42 -0600

    testing autoupdate and csv source change

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,18 @@
 from dash import Dash
 from dash import dash_table, html, dcc, Output, Input
-from layout import layout, get_callbacks
+import pandas as pd
+# from layout import layout, get_callbacks
 
 app = Dash(__name__)
 server = app.server
 
 # layout = html.Div([html.Header('test')])
+df = pd.read_csv('https://raw.githubusercontent.com/tlarson7/public_cod_stats_raw_data/main/final_stats.csv')
+table = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], sort_action="native")
+layout = html.Div([table])
 
 app.layout = layout
-get_callbacks(app)
+# get_callbacks(app)
 app.title = "Hayz Stats v0.1"
 
 # app.run_server(host='0.0.0.0', port='8050')
