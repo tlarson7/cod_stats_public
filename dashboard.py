6aca13b2742e96cb458e809ab20723c21e2d81c5
Tanner Larson <65421823+tlarson7@users.noreply.github.com>
2024-02-08 19:54:21 -0600

    testing layout and csv

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,14 @@
 from dash import Dash
 from dash import dash_table, html, dcc, Output, Input
-# from layout import layout, get_callbacks
+from layout import layout, get_callbacks
 
 app = Dash(__name__)
 server = app.server
 
-layout = html.Div([html.Header('test')])
+# layout = html.Div([html.Header('test')])
 
 app.layout = layout
-# get_callbacks(app)
+get_callbacks(app)
 app.title = "Hayz Stats v0.1"
 
 # app.run_server(host='0.0.0.0', port='8050')
